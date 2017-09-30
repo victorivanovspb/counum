b4dda415ac63a6af331e0a126d31683a2f3849aa
Victor Ivanov <victorivanov.spb@gmail.com>
2017-09-30 15:48:13 +0300

    Upd переделано решительно всё: js, стили, html и даже readme.md

--- js/main.js
+++ js/main.js
@@ -1,14 +1,32 @@
+function logEntry(message) {
+    $('#hst')
+        .find('ul.log li:eq(0)')
+        .before(`<li>${message}</li>`);
+}
 
-var prepareButtons = function() {
-    $("#btn1").click(function() {
-        console.log("=== BTN #1 ===\n");
-    });
-    $("#btnEnter").click(function() {
-        console.log("### ENTER ###\n");
-    });
-};
+function genNewTask() {
+    $('#tsk').html(
+        window.numberman
+            .gen()
+            .task
+    );
+}
+
+function btnPress(e) {
+    if (e.keyCode === 13) { // Enter
+        let $inp = $('#input-inp');
+        let curr = parseInt($inp.val().trim());
+        if (window.numberman.answer === curr) {
+            $inp.val('');
+            logEntry(curr);
+            genNewTask();
+        }
+    }
+}
 
 $( document ).ready(function() {
-    console.log("Document is ready.");
-    prepareButtons();
+    $('#input-inp').keypress(btnPress);
+
+    window['numberman'] = new Numberman();
+    genNewTask()
 });
--- readme.md
+++ readme.md
@@ -2,5 +2,4 @@
 
 [https://translate.google.com/#is/ru/counum](https://translate.google.com/#is/ru/counum)
 
-  - Bootstrap 4
   - JQuery
